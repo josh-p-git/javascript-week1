216255059fdd924483f9e7249e0cb41d9f0a101c
josh-p-git <joshua.penney91@gmail.com>
2023-01-26 15:22:02 +0000

    thermostat and testing complete

--- challenge4/thermostat.js
+++ challenge4/thermostat.js
@@ -10,17 +10,42 @@ class Thermostat {
     }
 
     up() {
-        return this.temperature += 1
+        if (this.temperature < this.maxTemp) {
+            return this.temperature += 1
+        } else {
+            return this.temperature
+        }
     }
 
     down() {
-        return this.temperature -= 1
+        if (this.temperature > this.minTemp) {
+            return this.temperature -= 1
+        } else {
+            return this.temperature
+        }
     }
+
+    setPowerSavingMode(mode) {
+        if (mode === true) {
+            this.maxTemp = 25
+        } else if (mode === false) {
+            this.maxTemp = 32
+        }
+    }
+
+    reset() {
+        this.temperature = 20;
+    }
+
+    energyUsage() {
+        if (this.temperature < 19 ) {
+            return 'low-usage'
+        } else if (this.temperature <= 25) {
+            return 'medium-usage'
+        } else {
+            return 'high-usage'
+        }
+    }    
 };
 
 module.exports = Thermostat;
-
-/*temp = new Thermostat();
-console.log(temp.getTemperature());
-console.log(temp.up());
-console.log(temp.down());*/
--- challenge4/thermostat.test.js
+++ challenge4/thermostat.test.js
@@ -1,6 +1,6 @@
 const Thermostat = require('./thermostat.js')
 
-describe('thermostat can be created and adjusted', () => {
+describe('thermostat can be created', () => {
     const temp = new Thermostat();
 
     test('returns starting temperature of 20', () => {
@@ -8,7 +8,7 @@ describe('thermostat can be created and adjusted', () => {
   });
 });
 
-describe('thermostat can be created adjusted', () => {
+describe('thermostat can be adjusted', () => {
     const temp = new Thermostat();
 
     test('temperature can be increased by 1', () => {
@@ -16,14 +16,78 @@ describe('thermostat can be created adjusted', () => {
   });
 });
 
-describe('thermostat can be created adjusted', () => {
+describe('thermostat can be adjusted', () => {
     const temp = new Thermostat();
     test('temperature can be decreased by 1', () => {
         expect(temp.down()).toEqual(19);
   });
 });
 
+describe('powersaving mode can be used', () => {
+    const temp = new Thermostat();
+    test('powersaving mode on sets max temp to 25 when enabled', () => {
+        temp.setPowerSavingMode(true);
+        for (let i = 0 ; i < 10 ; i++) {
+            temp.up();
+          };
+          expect(temp.getTemperature()).toEqual(25);
+  });
+});
 
+describe('powersaving mode can be used', () => {
+    const temp = new Thermostat();
+    test('powersaving mode off sets max temp to 32 when enabled', () => {
+        temp.setPowerSavingMode(false);
+        for (let i = 0 ; i < 15 ; i++) {
+            temp.up();
+          };
+          expect(temp.getTemperature()).toEqual(32);
+  });
+});
 
+describe('temperature cannot be set below 10', () => {
+    const temp = new Thermostat();
+    test('temperature cannot be set below 10', () => {
+        for (let i = 0 ; i < 15 ; i++) {
+            temp.down();
+          };
+          expect(temp.getTemperature()).toEqual(10);
+  });
+});
 
+describe('thermometer can be reset', () => {
+    const temp = new Thermostat();
+    test('temperature can be reset to 20', () => {
+        temp.down();
+        temp.reset();
+        expect(temp.getTemperature()).toEqual(20);
+  });
+});
 
+describe('thermometer repots energy usage', () => {
+    const temp = new Thermostat();
+    test('thermostat returns low-usage when temperature is < 18', () => {
+        for (let i = 0 ; i < 3 ; i++) {
+            temp.down();
+          };
+          expect(temp.energyUsage()).toEqual('low-usage');
+  });
+});
+
+describe('thermometer repots energy usage', () => {
+    const temp = new Thermostat();
+    test('thermostat returns medium-usage when temperature is less than <= 25', () => {
+          expect(temp.energyUsage()).toEqual('medium-usage');
+  });
+});
+
+describe('thermometer repots energy usage', () => {
+    const temp = new Thermostat();
+    test('thermostat returns high-usage when temperature is > 25', () => {
+        temp.setPowerSavingMode(false);
+        for (let i = 0 ; i < 7 ; i++) {
+            temp.up();
+          };
+          expect(temp.energyUsage()).toEqual('high-usage');
+  });
+});
