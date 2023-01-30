9558807b0242ee967c7aa10bd3558064ab0701da
josh-p-git <joshua.penney91@gmail.com>
2023-01-30 10:59:35 +0000

    weather setup

--- challenge6/index.js
+++ challenge6/index.js
@@ -1,1 +1,13 @@
 const apiKey = require('./apiKey');
+const city = 'London';
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
+
+let weatherData = null;
+
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((weatherData) => {
+    console.log(weatherData)
+  });
+
+console.log('Requesting weather data');
